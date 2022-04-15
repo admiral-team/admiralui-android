01bafd05f745263f20d73f73dc468c4bc8ced917
Evgeniy <EMigunov@inno.tech>
2022-04-15 13:39:27 +0300

    [FEATURE] - InformerSmallTest new screenshots

--- demo/src/androidTest/java/com/admiral/demo/views/InformerSmallTest.kt
+++ demo/src/androidTest/java/com/admiral/demo/views/InformerSmallTest.kt
@@ -4,12 +4,11 @@ import com.karumi.shot.ScreenshotTest
 import androidx.test.platform.app.InstrumentationRegistry
 import com.admiral.demo.ext.measureUnspecified
 import com.admiral.uikit.components.informer.InformerSmall
+import com.admiral.uikit.components.informer.InformerStyle
 import org.junit.Test
-
 
 class InformerSmallTest : ScreenshotTest {
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
-
 
     private fun InformerSmall.check() {
         measureUnspecified()
@@ -20,22 +19,53 @@ class InformerSmallTest : ScreenshotTest {
         )
     }
 
-    private fun checkInformerSmallProgrammatically(isEnabled: Boolean) {
+    private fun checkInformerSmallProgrammatically(isEnabled: Boolean, style: InformerStyle) {
         val informerSmall = InformerSmall(context).apply {
             this.isEnabled = isEnabled
+            this.informerStyle = style
+            this.info = "Text informer"
+            this.pointerBias = 0.9f
         }
         informerSmall.check()
     }
 
     // region check programmatically
     @Test
-    fun checkProgrammaticallyInformerSmallEnabledState() {
-        checkInformerSmallProgrammatically(isEnabled = true)
+    fun checkProgrammaticallyInformerSmallEnabledAttentionState() {
+        checkInformerSmallProgrammatically(isEnabled = true, style = InformerStyle.Attention)
+    }
+    @Test
+    fun checkProgrammaticallyInformerSmallEnabledSuccessState() {
+        checkInformerSmallProgrammatically(isEnabled = true, style = InformerStyle.Success)
+    }
+    @Test
+    fun checkProgrammaticallyInformerSmallEnabledErrorState() {
+        checkInformerSmallProgrammatically(isEnabled = true, style = InformerStyle.Error)
     }
 
     @Test
-    fun checkProgrammaticallyInformerSmallDisabledState() {
-        checkInformerSmallProgrammatically(isEnabled = false)
+    fun checkProgrammaticallyInformerSmallEnabledInfoState() {
+        checkInformerSmallProgrammatically(isEnabled = true, style = InformerStyle.Info)
+    }
+
+    @Test
+    fun checkProgrammaticallyInformerSmallDisabledAttentionState() {
+        checkInformerSmallProgrammatically(isEnabled = false, style = InformerStyle.Attention)
+    }
+
+    @Test
+    fun checkProgrammaticallyInformerSmallDisabledSuccessState() {
+        checkInformerSmallProgrammatically(isEnabled = false, style = InformerStyle.Success)
+    }
+
+    @Test
+    fun checkProgrammaticallyInformerSmallDisabledErrorState() {
+        checkInformerSmallProgrammatically(isEnabled = false, style = InformerStyle.Error)
+    }
+
+    @Test
+    fun checkProgrammaticallyInformerSmallDisabledInfoState() {
+        checkInformerSmallProgrammatically(isEnabled = false, style = InformerStyle.Info)
     }
     // endregion
 }
