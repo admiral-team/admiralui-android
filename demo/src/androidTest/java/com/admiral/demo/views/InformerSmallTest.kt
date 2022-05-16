f3f98351daa3c8e3182da07e6e282f768d4caafc
Evgeniy <EMigunov@inno.tech>
2022-05-16 13:00:25 +0300

    [FEATURE] - InformerSmall test by inflation

--- demo/src/androidTest/java/com/admiral/demo/views/InformerSmallTest.kt
+++ demo/src/androidTest/java/com/admiral/demo/views/InformerSmallTest.kt
@@ -9,11 +9,24 @@ import com.admiral.demo.ext.measureUnspecified
 import com.admiral.uikit.components.informer.InformerSmall
 import com.admiral.uikit.components.informer.InformerStyle
 import org.junit.Test
+import androidx.viewbinding.ViewBinding
 
 class InformerSmallTest : ScreenshotTest {
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
     private val layoutInflater = LayoutInflater.from(context)
     private val binding = TestViewInformerSmallBinding.inflate(layoutInflater)
+
+    private fun checkInformerSmallByInflation(
+        viewBinding: ViewBinding,
+        isEnabled: Boolean,
+        style: InformerStyle
+    ) {
+        with(viewBinding.root as InformerSmall) {
+            this.isEnabled = isEnabled
+            this.informerStyle = style
+            check()
+        }
+    }
 
     private fun InformerSmall.check() {
         measureUnspecified()
@@ -32,6 +45,79 @@ class InformerSmallTest : ScreenshotTest {
             this.pointerBias = 0.9f
         }
         informerSmall.check()
+    }
+
+    // region check by inflation
+    @Test
+    fun checkByInflationInformerSmallEnabledAttentionState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = true,
+            style = InformerStyle.Attention
+        )
+    }
+
+    @Test
+    fun checkByInflationInformerSmallEnabledSuccessState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = true,
+            style = InformerStyle.Success
+        )
+    }
+
+    @Test
+    fun checkByInflationInformerSmallEnabledErrorState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = true,
+            style = InformerStyle.Error
+        )
+    }
+
+    @Test
+    fun checkByInflationInformerSmallEnabledInfoState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = true,
+            style = InformerStyle.Info
+        )
+    }
+
+    @Test
+    fun checkByInflationInformerSmallDisabledAttentionalState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = false,
+            style = InformerStyle.Attention
+        )
+    }
+
+    @Test
+    fun checkByInflationInformerSmallDisabledSuccessState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = false,
+            style = InformerStyle.Success
+        )
+    }
+
+    @Test
+    fun checkByInflationInformerSmallDisabledErrorState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = false,
+            style = InformerStyle.Error
+        )
+    }
+
+    @Test
+    fun checkByInflationInformerSmallInfoState() {
+        checkInformerSmallByInflation(
+            viewBinding = binding,
+            isEnabled = false,
+            style = InformerStyle.Info
+        )
     }
 
     // region check programmatically
