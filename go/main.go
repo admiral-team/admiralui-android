895180c67f5d498c22b2c5e37a64a957d999f255
timbaton <timbaton@users.noreply.github.com>
2023-03-15 20:01:02 +0700

    [TECH] - add script task to upload library into nexus

--- go/main.go
+++ go/main.go
@@ -23,9 +23,28 @@ func main() {
 		githubToken := goDotEnvVariable("GITHUB_TOKEN")
 		tgToken := goDotEnvVariable("TELEGRAM_API_TOKEN")
 		client.ReleaseAndroid(githubToken, tgToken)
+	case "uploadNexus":
+		loadFilesToNexus()
 	default:
 		fmt.Println("Unknown command")
 	}
+}
+
+func loadFilesToNexus() {
+	loadConcreteFileToNexus("admiral-resources", "admiral-resources-5.6.0.aar")
+	loadConcreteFileToNexus("admiral-themes", "admiral-themes-5.6.0.aar")
+	loadConcreteFileToNexus("admiral-uikit", "admiral-uikit-5.6.0.aar")
+	loadConcreteFileToNexus("admiral-uikit-common", "admiral-uikit-common-5.6.0.aar")
+}
+
+func loadConcreteFileToNexus(artifactId string, fileName string) {
+	version := "5.6.0"
+	groupId := "admiralui-android"
+	extention := "aar"
+	nexusRepository := goDotEnvVariable("NEXUS_URL")
+	nexusUsername := goDotEnvVariable("NEXUS_USERNAME")
+	nexusPassword := goDotEnvVariable("NEXUS_PASSWORD")
+	client.UploadNexus(groupId, artifactId, version, "../libs/"+fileName, extention, nexusRepository, nexusUsername, nexusPassword)
 }
 
 func goDotEnvVariable(key string) string {
