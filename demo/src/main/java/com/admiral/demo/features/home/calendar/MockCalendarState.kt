cfc9410592d616d7db7229a41af374e6f1245af1
Akhmedov Murad Muslimovich [4097679] <makhmedov@inno.tech>
2024-01-16 15:04:53 +0300

    [BUGFIX] - changed the method of obtaining mock data to the correct one

--- demo/src/main/java/com/admiral/demo/features/home/calendar/MockCalendarState.kt
+++ demo/src/main/java/com/admiral/demo/features/home/calendar/MockCalendarState.kt
@@ -4,27 +4,30 @@ import com.admiral.uikit.components.calendar.common.CalendarState
 import com.admiral.uikit.components.calendar.common.DisabledDaysInfo
 import com.admiral.uikit.components.calendar.common.SelectionMode
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.YearMonth
+
+private val currentYear = LocalDateTime.now().year
 
 @Suppress("MagicNumber")
 val MockCalendarState = CalendarState(
-    initialYearMonth = YearMonth.of(2021, 1),
+    initialYearMonth = YearMonth.of(currentYear, 1),
     markedDays = listOf(
-        LocalDate.of(2021, 1, 5),
-        LocalDate.of(2021, 1, 6),
-        LocalDate.of(2021, 1, 7)
+        LocalDate.of(currentYear, 1, 5),
+        LocalDate.of(currentYear, 1, 6),
+        LocalDate.of(currentYear, 1, 7)
     ),
     disabledDaysInfo = DisabledDaysInfo(
         disabledDays = listOf(
-            LocalDate.of(2021, 1, 15),
-            LocalDate.of(2021, 1, 16),
-            LocalDate.of(2021, 1, 17)
+            LocalDate.of(currentYear, 1, 15),
+            LocalDate.of(currentYear, 1, 16),
+            LocalDate.of(currentYear, 1, 17)
         )
     ),
     selectionMode = SelectionMode.IntervalSelection,
     highlightedDays = listOf(
-        LocalDate.of(2021, 1, 19),
-        LocalDate.of(2021, 1, 20),
-        LocalDate.of(2021, 1, 21)
+        LocalDate.of(currentYear, 1, 19),
+        LocalDate.of(currentYear, 1, 20),
+        LocalDate.of(currentYear, 1, 21)
     )
 )
