576b8362cf8ccf9289725180cf99c9aac8eba992
Бадретдинов Тимур Маратович (4043133) <TBadretdinov@inno.tech>
2024-01-26 01:43:59 +0300

    [BUGFIX-50] - Compose - fix theme changing bug

--- admiral-uikit-compose/src/main/java/com/admiral/uikit/compose/Switcher.kt
+++ admiral-uikit-compose/src/main/java/com/admiral/uikit/compose/Switcher.kt
@@ -3,8 +3,6 @@ package com.admiral.uikit.compose
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.admiral.themes.compose.ThemeManagerCompose
@@ -14,33 +12,45 @@ import com.admiral.uikit.core.foundation.ColorState
 fun switcher(
     modifier: Modifier = Modifier,
     checked: Boolean = false,
+    onCheckedChanged: ((Boolean) -> Unit)? = null,
     isEnabled: Boolean = true,
     thumbColor: ColorState? = null,
     trackColor: ColorState? = null
-): Boolean {
+) {
     val theme = ThemeManagerCompose.theme.value
-
-    val (value, setValue) = remember { mutableStateOf(checked) }
 
     Switch(
         modifier = modifier,
-        checked = value, onCheckedChange = setValue,
+        checked = checked,
+        onCheckedChange = {
+            onCheckedChanged?.invoke(it)
+        },
         enabled = isEnabled,
         colors = SwitchDefaults.colors(
-            checkedThumbColor = Color(thumbColor?.checkedEnabled ?: theme.palette.elementStaticWhite),
-            uncheckedThumbColor = Color(thumbColor?.normalEnabled ?: theme.palette.elementStaticWhite),
-            disabledCheckedThumbColor = Color(thumbColor?.checkedDisabled ?: theme.palette.elementStaticWhite),
-            disabledUncheckedThumbColor = Color(thumbColor?.normalDisabled ?: theme.palette.elementStaticWhite),
+            checkedThumbColor = Color(
+                thumbColor?.checkedEnabled ?: theme.palette.elementStaticWhite
+            ),
+            uncheckedThumbColor = Color(
+                thumbColor?.normalEnabled ?: theme.palette.elementStaticWhite
+            ),
+            disabledCheckedThumbColor = Color(
+                thumbColor?.checkedDisabled ?: theme.palette.elementStaticWhite
+            ),
+            disabledUncheckedThumbColor = Color(
+                thumbColor?.normalDisabled ?: theme.palette.elementStaticWhite
+            ),
 
             checkedTrackColor = Color(trackColor?.checkedEnabled ?: theme.palette.elementAccent),
             uncheckedTrackColor = Color(trackColor?.normalEnabled ?: theme.palette.elementPrimary),
-            disabledCheckedTrackColor = Color(trackColor?.checkedDisabled ?: theme.palette.elementAccent),
-            disabledUncheckedTrackColor = Color(trackColor?.normalDisabled ?: theme.palette.elementPrimary),
+            disabledCheckedTrackColor = Color(
+                trackColor?.checkedDisabled ?: theme.palette.elementAccent
+            ),
+            disabledUncheckedTrackColor = Color(
+                trackColor?.normalDisabled ?: theme.palette.elementPrimary
+            ),
 
             checkedTrackAlpha = 1f,
             uncheckedTrackAlpha = 1f,
         )
     )
-
-    return value
 }
