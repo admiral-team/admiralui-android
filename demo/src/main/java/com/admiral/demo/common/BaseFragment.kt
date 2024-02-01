a103bc93be61a6ddbbb3bbd15c32f18b58b10b00
Бадретдинов Тимур Маратович (4043133) <TBadretdinov@inno.tech>
2024-02-01 12:41:12 +0300

    [FEATURE-40] - hide keyboard on tap

--- demo/src/main/java/com/admiral/demo/common/BaseFragment.kt
+++ demo/src/main/java/com/admiral/demo/common/BaseFragment.kt
@@ -1,5 +1,14 @@
 package com.admiral.demo.common
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -23,5 +32,55 @@ abstract class BaseFragment(@LayoutRes layoutId: Int) : Fragment(layoutId) {
     fun showBackButton() {
         (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         (requireActivity() as AppCompatActivity).supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        hideKeyboardOnClick(view.rootView)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun hideKeyboardOnClick(view: View) {
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (view !is EditText) {
+            var startClickTime: Long = 0
+
+            view.setOnTouchListener { _, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    startClickTime = System.currentTimeMillis()
+                } else if (event.action == MotionEvent.ACTION_UP) {
+                    if ((System.currentTimeMillis() - startClickTime) < ViewConfiguration.getTapTimeout() * TAP_TIMEOUT_CONST) {
+                        hideKeyboard(requireActivity().currentFocus)
+                    }
+                }
+                false
+            }
+        }
+
+        //If a layout is a container, iterate over children and seed recursion.
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                val innerView = view.getChildAt(i)
+                hideKeyboardOnClick(innerView)
+            }
+        }
+    }
+
+    private fun hideKeyboard(view: View?) {
+        if (view == null) {
+            return
+        }
+        val imm =
+            view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (!imm.isActive) {
+            return
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        view.clearFocus()
+    }
+
+    private companion object {
+        const val TAP_TIMEOUT_CONST = 1.8
     }
 }
