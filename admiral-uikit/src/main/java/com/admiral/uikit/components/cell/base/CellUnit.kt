de47bf8a988fde46e79968abf00f4547196e4c63
Бадретдинов Тимур Маратович (4043133) <TBadretdinov@inno.tech>
2024-02-28 19:59:26 +0300

    [FEATURE-21] - remove deprecated methods

--- admiral-uikit/src/main/java/com/admiral/uikit/components/cell/base/CellUnit.kt
+++
@@ -1,6 +0,0 @@
-package com.admiral.uikit.components.cell.base
-
-@Deprecated("Use com.admiral.uikit.core.components.cell.base.CellUnit instead")
-interface CellUnit {
-    var unitType: CellUnitType
-}
